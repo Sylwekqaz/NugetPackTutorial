81912fca288cbfab28c78a37cd05f5f687ec45d9
Sylwester Turski <s.turski@hypermedia.pl>
2017-08-23 11:07:29 +0200

    fix AssemblyInfo and nuspec information

--- src/ConfigReader.Implementations/Properties/AssemblyInfo.cs
+++ src/ConfigReader.Implementations/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConfigReader.Implementations")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Its implementation for configuration reader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ConfigReader.Implementations")]
